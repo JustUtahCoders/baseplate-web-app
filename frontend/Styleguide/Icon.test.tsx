3ff3d53e9cbb30b8250812c83d1f0848aaf4a7e3
joeldenning <joeldenning@gmail.com>
2022-03-19 16:53:55 -0600

    Fix tests

--- frontend/Styleguide/Icon.test.tsx
+++ frontend/Styleguide/Icon.test.tsx
@@ -4,25 +4,41 @@ import { Icon, IconVariant } from "./Icon";
 
 describe("<Icon />", () => {
   it(`always renders a <title>`, () => {
+    let w;
+
     Object.keys(IconVariant).forEach((iconVariant) => {
-      const w = render(<Icon variant={iconVariant} />);
+      if (w) {
+        w.unmount();
+      }
+
+      w = render(<Icon variant={iconVariant as IconVariant} />);
 
       expect(w.baseElement.querySelector("title")).toBeInTheDocument();
 
-      w.rerender(<Icon variant={iconVariant} alt="Donkey Kong" />);
+      w.rerender(
+        <Icon variant={iconVariant as IconVariant} alt="Donkey Kong" />
+      );
 
       expect(w.queryByTitle("Donkey Kong")).toBeInTheDocument();
     });
   });
 
   it(`respects the size prop`, () => {
+    let w;
+
     Object.keys(IconVariant).forEach((iconVariant) => {
-      const w = render(<Icon variant={iconVariant} alt="the image" />);
+      if (w) {
+        w.unmount();
+      }
+
+      w = render(<Icon variant={iconVariant as IconVariant} alt="the image" />);
       let svgEl = w.getByTitle("the image").parentElement;
       expect(svgEl.getAttribute("height")).toBe(`16px`);
       expect(svgEl.getAttribute("width")).toBe(`16px`);
 
-      w.rerender(<Icon variant={iconVariant} alt="the image" size={26} />);
+      w.rerender(
+        <Icon variant={iconVariant as IconVariant} alt="the image" size={26} />
+      );
       svgEl = w.getByTitle("the image").parentElement;
       expect(svgEl.getAttribute("height")).toBe(`26px`);
       expect(svgEl.getAttribute("width")).toBe(`26px`);
